aa48ad7adc5c1193893a1e8b2f5f75fcb16cfe60
Lenoir Seibert <siwebbr@gmail.com>
2021-09-07 21:09:13 -0300

    upd

---
+++ aula-1/aula1.js
@@ -0,0 +1,41 @@
+/*
+ * Conceitos de Codigo
+ * -> Sentença de codigo <-
+ * -> Bloco de Codigo <-
+ */
+
+/*Abaixo são 3 sentenças de Codigo
+Podendo elas ocupar mais que uma linha.
+Uma comparação... seria o paragrafo que é
+formado por varias palavras*/
+// console.log('teste 1')
+// console.log('teste 2')
+// console.log('teste 3')
+
+/**Comentarios */
+// Linha
+/**
+ *
+ * Bloco
+ *
+ * */
+
+/**
+ * Bloco de código no javascript
+ *  {
+ *      {
+ *        sentença de codigo
+ *      }
+ *  }
+ */
+{
+  console.log('bloco de código no javascript')
+}
+{
+  {
+    console.log('bloco de código no javascript')
+  }
+  {
+    console.log('bloco de código no javascript')
+  }
+}
