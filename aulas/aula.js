738867633614ecb71dd75902f817f8d770b5a148
Lenoir Seibert <siwebbr@gmail.com>
2021-09-09 04:27:34 -0300

    upd

--- aulas/aula.js
+++ aulas/aula.js
@@ -6,6 +6,37 @@
 /**Desafio  aritmeticos
  * F − 32 / 9 * 5
  */
-let f = 100
-const c = ((f - 32) / 9) * 5
-console.log(c + 'C°')
+// let f = 100
+// const c = ((f - 32) / 9) * 5
+// console.log(c + 'C°')
+
+/**Desafio operadores relacionais */
+// let a = 1
+// let b = 2
+
+// console.log(a < b)
+// console.log(a <= b)
+// console.log(a != b)
+// console.log(a !== b)
+// console.log(a + a === b)
+
+// console.log(a > b)
+// console.log(a >= b)
+// console.log(a == b)
+// console.log(a === b)
+// console.log(a + a !== b)
+
+/**atribuição */
+let x = 10
+x = x + 6
+x = x - 1
+console.log(x)
+
+let y = 20
+y += 10
+console.log(y)
+
+let w = 'texto'
+w += ', mais um pouco de texto,'
+w += ' oloquinho meu!!!!'
+console.log(w)
