c8411b5ffb47a85ef85d90a066da9d20bec036fe
Lenoir Seibert <siwebbr@gmail.com>
2021-09-09 09:27:56 -0300

    upd

--- aulas/aula.js
+++ aulas/aula.js
@@ -72,6 +72,6 @@
 // console.log(saudacao)
 
 /**Operador ternário */
-const hora = 11
-const saudacao = hora <= 11 ? 'Bom dia' : hora <= 17 ? 'Boa Tarde' : 'Boa noite'
-console.log(saudacao)
+// const hora = 11
+// const saudacao = hora <= 11 ? 'Bom dia' : hora <= 17 ? 'Boa Tarde' : 'Boa noite'
+// console.log(saudacao)
