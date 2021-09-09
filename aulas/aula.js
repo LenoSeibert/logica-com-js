a46a71e800282ffef01f32579d6833dbc00e2879
Lenoir Seibert <siwebbr@gmail.com>
2021-09-09 17:00:02 -0300

    upd

--- aulas/aula.js
+++ aulas/aula.js
@@ -93,15 +93,32 @@
 
 // console.log('Fim!')
 
-const nota = 9
+// const nota = 9
 
-if (nota >= 9) {
-  console.log('Quadro de Honra!')
-  console.log('Parabéns!')
-} else if (nota >= 5) {
-  console.log('Voce precisa melhorar!')
-} else {
-  console.log('Você está em apuros')
+// if (nota >= 9) {
+//   console.log('Quadro de Honra!')
+//   console.log('Parabéns!')
+// } else if (nota >= 5) {
+//   console.log('Voce precisa melhorar!')
+// } else {
+//   console.log('Você está em apuros')
+// }
+
+// console.log('Fim!')
+
+/**Desafio IF */
+const nota = 4.9
+
+if (nota <= 10 && nota >= 9) console.log('A')
+
+if (nota < 9 && nota >= 7) console.log('B')
+
+if (nota < 7 && nota >= 5) console.log('C')
+
+if (nota < 5 && nota >= 4.5) console.log('D')
+
+if (nota >= 0 && nota < 4.5) {
+  console.log('F')
+} else if (nota > 10 || nota < 0) {
+  console.log('Nota não existe!')
 }
-
-console.log('Fim!')
