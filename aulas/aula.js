c06a4def7760c6b26f201b5078b1070228dd9cca
Lenoir Seibert <siwebbr@gmail.com>
2021-09-12 16:32:39 -0300

    upd

--- aulas/aula.js
+++ aulas/aula.js
@@ -257,12 +257,39 @@
 // #####
 // ######
 
-let linha = ''
-for (let i = 1; i <= 6; i++) {
-  linha += '#'
-  console.log(linha)
+// let linha = ''
+// for (let i = 1; i <= 6; i++) {
+//   linha += '#'
+//   console.log(linha)
+// }
+
+// for (let s = '#'; s != '#######'; s += '#') {
+//   console.log(s)
+// }
+let nota = 10
+
+switch (Math.ceil(nota)) {
+  case 10:
+  case 9:
+  case 8:
+  case 7:
+    console.log('Parabéns! Você Passou!')
+    break
+  case 6:
+  case 5:
+    console.log('Recuperação!')
+    break
+  case 4:
+  case 3:
+  case 2:
+    console.log('Reprovado! ')
+    break
+  case 1:
+  case 0:
+    console.log('Muito Reprovado!')
+    break
+  default:
+    console.log('Nota inválida!')
 }
 
-for (let s = '#'; s != '#######'; s += '#') {
-  console.log(s)
-}
+console.log('Fim!')
