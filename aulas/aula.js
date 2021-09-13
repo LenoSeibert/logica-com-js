c35f50ac5ed7475e01b41523946549c0eeb58d87
Lenoir Seibert <siwebbr@gmail.com>
2021-09-13 06:51:40 -0300

    Funçoes

---
+++ aulas/aula.js
@@ -0,0 +1,64 @@
+/**
+ * Funcoes
+ *
+ */
+//Sem função
+// const d1 = 3
+// const m1 = 7
+// const a1 = 2021
+
+// console.log(`Dia: ${d1}`)
+// console.log(`Mês: ${m1}`)
+// console.log(`Ano: ${a1}`)
+
+// const d2 = 23
+// const m2 = 12
+// const a2 = 2022
+
+// console.log(`Dia: ${d2}`)
+// console.log(`Mês: ${m2}`)
+// console.log(`Ano: ${a2}`)
+
+//Com função
+// const d1 = 3
+// const m1 = 7
+// const a1 = 2021
+
+// function exibirData1() {
+//   console.log(`${d1}/${m1}/${a1}`)
+// }
+
+// const d2 = 23
+// const m2 = 12
+// const a2 = 2022
+
+// function exibirData2() {
+//   console.log(`${d2}/${m2}/${a2}`)
+// }
+
+// exibirData1()
+// exibirData2()
+// exibirData1()
+
+// exibirData2()
+// exibirData1()
+// exibirData2()
+const d1 = 3
+const m1 = 7
+const a1 = 2021
+
+const d2 = 23
+const m2 = 12
+const a2 = 2022
+
+function exibirData(d, m, a) {
+  console.log(`${d}/${m}/${a}`)
+}
+
+exibirData(d1, m1, a1)
+exibirData(d1, m1, a1)
+exibirData(d1, m1, a1)
+
+exibirData(d2, m2, a2)
+exibirData(d2, m2, a2)
+exibirData(d2, m2, a2)
